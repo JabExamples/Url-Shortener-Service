ea9fe12af3e3c7ddd9bac53529dfdb0c0b53fa6c
Jack <git>
2020-04-28 21:40:40 +0300

    Adding tests for project

---
+++ sw-developer-assignment-master/UrlShortener/UrlShortenerAppTests/Controllers/ShortenUrlControllerTests.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace UrlShortenerAppTests
+{
+    public class ShortenUrlControllerTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ sw-developer-assignment-master/UrlShortener/UrlShortenerAppTests/Services/ShortenUrlService/CleanUriUrlShortenerServiceTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace UrlShortenerAppTests.Services.ShortenUrlService
+{
+    public class CleanUriUrlShortenerServiceTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
